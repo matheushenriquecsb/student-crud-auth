cc2e87864f363d689fd00d4a8e039b41d1ba796f
unknown <matheus.mhg2@gmail.com>
2023-05-24 10:50:53 -0300

    feat: add migration student and model

---
+++ app/Models/Student.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+
+class Student extends Model
+{
+    use HasFactory, HasApiTokens;
+    protected $fillable = ["name" , "email" , "phone_no" , "gender" , "age"];
+
+}
---
+++ database/migrations/2023_05_24_134509_create_students_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('email', 100);
+            $table->integer('age')->unsigned();;
+            $table->string('phone', 20);
+            $table->enum('gender', ["male", "female"]);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
